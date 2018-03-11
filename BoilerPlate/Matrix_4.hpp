e3501153566f20873681fb9b783eb947241ca5a0
unknown <raul.arturo798@gmail.com>
2018-03-10 22:48:48 -0400

    added matrix_4 constructors

--- BoilerPlate/Matrix_4.hpp
+++ BoilerPlate/Matrix_4.hpp
@@ -1,15 +1,24 @@
 #pragma once
+#ifndef MATRIX_4_HPP
+#define MATRIX_4_HPP
+
 
 namespace engine
 {
-	class Matrix_4
+	class matrix_4
 	{
 	public:
-		Matrix_4();
-		Matrix_4(float, float, float, float, float, float, float, float, 
-			float, float, float, float, float, float, float, float);
-		Matrix_4(float pArray[]);
+		//constructors
+		matrix_4();
+		matrix_4(float pArray[15]);
+		matrix_4(float pValue11, float pValue21, float pValue31, float pValue41,
+				 float pValue12, float pValue22, float pValue32, float pValue42,
+				 float pValue13, float pValue23, float pValue33, float pValue43,
+				 float pValue14, float pValue24, float pValue34, float pValue44);
+		
 	private:
 		float mMatrix[4][4];
 	};
 }
+
+#endif
