b8b9994a609882e50cb539c7a87814a7d1d102f8
ericksaga <erickmedina09@gmail.com>
2018-03-10 22:06:51 -0400

    now with the content

--- BoilerPlate/Matrix_4.hpp
+++ BoilerPlate/Matrix_4.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+namespace engine
+{
+	class Matrix_4
+	{
+	public:
+		Matrix_4();
+		Matrix_4(float, float, float, float, float, float, float, float, 
+			float, float, float, float, float, float, float, float);
+		Matrix_4(float pArray[]);
+	private:
+		float mMatrix[4][4];
+	};
+}
