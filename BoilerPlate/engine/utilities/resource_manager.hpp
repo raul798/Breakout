43b6fff75754571bc84bc2947d9be904146a302b
unknown <raul.arturo798@gmail.com>
2018-03-23 16:42:25 -0300

    deleted unnecessary files

--- BoilerPlate/engine/utilities/resource_manager.hpp
+++ BoilerPlate/engine/utilities/resource_manager.hpp
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef RESOURCE_MANAGER_HPP
+#define RESOURCE_MANAGER_HPP
+
+namespace engine
+{
+	namespace utilities
+	{
+		class resource_manager
+		{
+		public:
+			//costructor
+			resource_manager();
+			resource_manager();
+
+		private:
+		};
+	}
+}
+
+#endif
