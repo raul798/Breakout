2c08143d431249bcb19f639191caedc07868ac46
unknown <raul.arturo798@gmail.com>
2018-03-27 23:50:53 -0300

    fixes to ball constructor

--- BoilerPlate/engine/core/ball.cpp
+++ BoilerPlate/engine/core/ball.cpp
@@ -4,26 +4,8 @@ namespace engine
 {
 	namespace core
 	{
-		ball::ball(){
-			// make ball, need formula for making a circle
-			//for (int i = 0; i < 800; i+=8)
-			//{
-				/*
-				//positions
-				mBallIndices[i + 0] = cos(2 * 3.14 * i / 300.0);
-				mBallIndices[i + 1] = sin(2 * 3.14*i / 300.0);
-				mBallIndices[i + 2] = 0.0f;
-
-				//colors
-				mBallIndices[i + 4] = 1.0f;
-				mBallIndices[i + 5] = 0.0f;
-				mBallIndices[i + 6] = 0.0f;
-
-				//textures
-				mBallIndices[i + 7] = 1.0f;
-				mBallIndices[i + 8] = 0.0f;
-				*/
-			//}
+		ball::ball()
+		{
 			mTextureIndex = 1;
 		}
 
