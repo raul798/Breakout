3461a3dd230fee6dbab5bf87a5a15ff6d4bd3bfb
unknown <raul.arturo798@gmail.com>
2018-03-28 15:57:44 -0300

    indentation fix

--- BoilerPlate/vertex.glsl
+++ BoilerPlate/vertex.glsl
@@ -10,7 +10,8 @@ layout (location = 2) in vec2 aTexCoord;
 out vec4 ourColor;
 out vec2 TexCoord;
 
-void main() {
+void main() 
+{
 	gl_Position.xyz = aPos;
 	gl_Position.w = 1.0;
 	ourColor = aColor;
