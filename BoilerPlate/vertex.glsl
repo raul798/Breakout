4efcd5a9fd05379b74af386e28d65e6b90f7c9b3
unknown <raul.arturo798@gmail.com>
2018-03-28 00:08:55 -0300

    fixed colors to texture

--- BoilerPlate/vertex.glsl
+++ BoilerPlate/vertex.glsl
@@ -2,12 +2,12 @@
 
 // Input vertex data (attributes)
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec4 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
 
 // Output vertex data
-out vec3 ourColor;
+out vec4 ourColor;
 out vec2 TexCoord;
 
 void main() {
